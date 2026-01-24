42eb4fcd0ef0ab62edd8a08257dd0024db4c158a
PIYUSH MISHRA <piyushhmishraaa@gmail.com>
2026-01-24 21:14:26 +0530

    db setup initiated

---
+++ backend/src/lib/db.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose"
+import { ENV } from "./env.js"
+
+
+export const connectDB = async()=>{
+    try{
+        const conn = await mongoose.connect(ENV.DB_URL)
+        
+        console.log("Connected to mongoDB:",conn.connection.host)
+    }
+    catch(error){
+        console.error("Error connecting to DB",error)
+        process.exit(1)
+    }
+};
