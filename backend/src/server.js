814303cf6c9717f5f7cd61ef883bfddad6fe12c4
PIYUSH MISHRA <piyushhmishraaa@gmail.com>
2026-01-24 16:25:27 +0530

    deployment config added

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,16 +1,28 @@
 import express from "express"
-
-import { ENV } from "./lib/env";
+import path from "path"
+import { ENV } from "./lib/env.js";
 
 
 
 const app =  express()
-
-console.log(ENV.PORT);
+const __dirname = path.resolve();
+const PORT = ENV.PORT|| 5000;
+console.log(PORT);
 console.log(ENV.DB_URL)
-app.get("/",(req,res)=>{
+app.get("/health",(req,res)=>{
     res.status(200).json({msg:"success from app"})
 })
-app.listen(ENV.PORT,()=>{
-    console.log("listening on port ENV.PORT")
+app.get("/books",(req,res)=>{
+    res.status(200).json({msg:"this is books route"})
+})
+
+if(ENV.NODE_ENV==="production"){
+    app.use(express.static(path.join(__dirname,"../frontend/dist")))
+    app.get("/{*any}",(req,res)=>{
+        res.sendFile(path.join(__dirname,"../frontend","dist","index.html"))
+    });
+}
+
+app.listen(PORT,()=>{
+    console.log(`listening on port ${PORT}`)
 })
